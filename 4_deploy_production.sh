11abdb10337959fee5b2b4ccf4c34c5063147482
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-09-01 17:58:04 -0600

    Cleanup Ansible work & rename

--- 4_deploy_production.sh
+++ 4_deploy_production.sh
@@ -6,7 +6,7 @@ ansible-galaxy install -r requirements.yml
 
 echo '--------- Generate a Host Factory Token ------------'
 api_key=$(docker-compose exec conjur rails r "print Credentials['cucumber:user:admin'].api_key")
-hf_token=$(docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+hf_token=$(docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   hostfactory tokens create --duration-minutes=5 production/myapp | jq -r '.[0].token')
 
 echo '--------- Run Playbook ------------'
