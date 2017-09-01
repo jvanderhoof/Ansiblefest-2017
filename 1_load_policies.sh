039220787c0d6a6d8ba24582697e6155b7b43a45
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-09-01 17:25:37 -0600

    Update container name based on previous refactor

--- 1_load_policies.sh
+++ 1_load_policies.sh
@@ -3,20 +3,20 @@
 api_key=$(docker-compose exec conjur rails r "print Credentials['cucumber:user:admin'].api_key")
 
 echo '--------- Load Conjur Policy ------------'
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   policy load --replace root /src/policy/users.yml
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   policy load root /src/policy/policy.yml
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   policy load staging /src/policy/apps/myapp.yml
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   policy load staging /src/policy/apps/myapp.yml
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   policy load production /src/policy/apps/myapp.yml
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   policy load root /src/policy/apps/myapp_grants.yml
--- 2_set_secrets.sh
+++ 2_set_secrets.sh
@@ -3,20 +3,20 @@
 api_key=$(docker-compose exec conjur rails r "print Credentials['cucumber:user:admin'].api_key")
 
 echo '--------- Set Secrets defined in Policy ------------'
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   variable values add staging/myapp/database/username foo_staging_user
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   variable values add staging/myapp/database/password "$(openssl rand -hex 12)"
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   variable values add staging/myapp/stripe/private_key "$(openssl rand -hex 60)"
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   variable values add production/myapp/database/username foo_production_user
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   variable values add production/myapp/database/password "$(openssl rand -hex 12)"
 
-docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cli \
+docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key conjur_cli \
   variable values add production/myapp/stripe/private_key "$(openssl rand -hex 60)"
