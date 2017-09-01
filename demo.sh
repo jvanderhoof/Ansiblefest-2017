50d32ec79aa48cee7b6086705b8183d8224cac72
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-09-01 17:15:01 -0600

    Renamed `run` to `demo`, show logs while running, cleanup on exit

---
+++ demo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+function finish {
+  echo 'Removing demo environment'
+  echo '---'
+  docker-compose down -v
+}
+trap finish EXIT
+
+function main() {
+  docker-compose up --build -d conjur conjur_cli myapp_staging myapp_production
+  docker-compose scale myapp_staging=2 myapp_production=4
+  docker-compose logs -f
+}
+
+main
