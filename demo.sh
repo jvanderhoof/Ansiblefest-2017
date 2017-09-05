380ef3a87478da248d433ee49e2ffff6d6cf7ae8
Dustin Collins <dustinrcollins@gmail.com>
2017-09-05 12:27:19 -0500

    Use a virtualenv to operate ansible

--- demo.sh
+++ demo.sh
@@ -10,7 +10,7 @@ trap finish EXIT
 function main() {
   docker-compose up --build -d conjur conjur_cli myapp_staging myapp_production
   docker-compose scale myapp_staging=2 myapp_production=4
-  docker-compose logs -f
+  docker-compose logs -f conjur myapp_staging myapp_production
 }
 
 main
